49e2c8c00508bb05c03cd44e8aef977a91b449f7
hervedav <davia.herve@rte-france.com>
2024-08-06 13:20:15 +0200

    Setup asto.config.js for deploying to site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://hervedavid.github.io',
+	integrations: [
+		mdx(), 
+		sitemap()
+	],
 });
